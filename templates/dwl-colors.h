4b11fbbfcb50d772f1adb8d9aa6442c33b23c09c
λmolinae <65742076+amolinae06@users.noreply.github.com>
2025-04-16 18:53:22 +0000

    simplified dwl color template

--- templates/dwl-colors.h
+++ templates/dwl-colors.h
@@ -1,14 +1,6 @@
 #define WALLPAPER "%%wallpaper%%"
-/* Taken from https://github.com/djpohly/dwl/issues/466 */
-#define COLOR(hex)    { ((hex >> 24) & 0xFF) / 255.0f, \
-                        ((hex >> 16) & 0xFF) / 255.0f, \
-                        ((hex >> 8) & 0xFF) / 255.0f, \
-                        (hex & 0xFF) / 255.0f }
 
 static const float rootcolor[]             = COLOR(0x%%color0.hex%%ff);
-static uint32_t colors[][3]                = {
-	/*               fg          bg          border    */
-	[SchemeNorm] = { 0x%%color15.hex%%ff, 0x%%color0.hex%%ff, 0x%%color8.hex%%ff },
-	[SchemeSel]  = { 0x%%color15.hex%%ff, 0x%%color2.hex%%ff, 0x%%color1.hex%%ff },
-	[SchemeUrg]  = { 0x%%color15.hex%%ff, 0x%%color1.hex%%ff, 0x%%color2.hex%%ff },
-};
+static const float bordercolor[]           = COLOR(0x%%color2.hex%%ff);
+static const float focuscolor[]            = COLOR(0x%%color2.hex%%ff);
+static const float urgentcolor[]           = COLOR(0x%%color1.hex%%ff);
